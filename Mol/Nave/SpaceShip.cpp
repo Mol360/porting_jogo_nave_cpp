621a1df608c1197a295ff96197a2495a1aa28fbc
Marcelo de Oliveira Lopes <mol360@gmail.com>
2018-11-01 18:02:52 -0300

    * Utilização de objeto base.

--- Mol/Nave/SpaceShip.cpp
+++ Mol/Nave/SpaceShip.cpp
@@ -1,11 +1,11 @@
 #include "SpaceShip.h"
 
 void SpaceShip::setShipName(std::string n_ship_name){
-	this->ship_name = n_ship_name;
+	this->object_name = n_ship_name;
 }
 
 std::string SpaceShip::getShipName(){
-	return this->ship_name;    
+	return this->object_name;    
 }
 
 int SpaceShip::getLife(){
@@ -26,61 +26,28 @@ int SpaceShip::gotShot(){
 
 void SpaceShip::load(){
 	//SDL_Surface * ship_obj;
-	if(this->ship_image == ""){
-		this->ship_image = "nave_player.png";
+	if(this->object_image == ""){
+		this->object_image = "nave_player.png";
 	}
-	std::string tmp_image = "imgs/"+this->ship_image;
-	this->ship_obj = IMG_Load(tmp_image.c_str()); // Carrega a imagem
+	ObjectBase::load();
 	this->life = 100;
 	this->shot_value = 20;
-	this->ship_name = "Nave";
+	this->object_name = "Nave";
 	this->vel_x = 0;
 	this->vel_y = 0;
 	this->dest = {0,0,0,0}; // Destino da imagem no screen
 }
 
 std::string SpaceShip::getImageShip(){
-    return this->ship_image;
+    return this->object_image;
 }
 
 void SpaceShip::setImageShip(std::string n_ship_image){
-	this->ship_image = n_ship_image;
+	this->object_image = n_ship_image;
 }
 
 void SpaceShip::update(){
 	
-}
-
-void SpaceShip::draw(){
-	SDL_BlitSurface(this->ship_obj, NULL, this->screen, &this->dest); // Blita a imagem em dest
-}
-
-void SpaceShip::setScreen(SDL_Surface * n_screen){
-	this->screen = n_screen;
-}
-
-int SpaceShip::getPosX(){
-        return this->dest.x;
-}
-
-int SpaceShip::getPosY(){
-	return this->dest.y;
-}
-
-void SpaceShip::setPosX(int n_pos_x){
-	this->dest.x = n_pos_x;
-}
-
-void SpaceShip::setPosY(int n_pos_y){
-	this->dest.y = n_pos_y;
-}
-
-void SpaceShip::moveX(int n_vel_x){
-        this->dest.x = this->dest.x + n_vel_x;
-}
-
-void SpaceShip::moveY(int n_vel_y){
-        this->dest.y = this->dest.y + n_vel_y;
 }
 
 void SpaceShip::setVelY(int n_vel_y){
@@ -89,14 +56,6 @@ void SpaceShip::setVelY(int n_vel_y){
 
 void SpaceShip::setVelX(int n_vel_x){
 	this->vel_x = n_vel_x;
-}
-
-int SpaceShip::getHeight(){
-	return this->ship_obj->h;
-}
-
-int SpaceShip::getWidth(){
-	return this->ship_obj->w;
 }
 
 void SpaceShip::addEnemy(SpaceShip *n_enemy){
--- Mol/Nave/SpaceShip.h
+++ Mol/Nave/SpaceShip.h
@@ -3,32 +3,19 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <vector>
+#include "../ObjectBase.h"
 
-class SpaceShip{
+class SpaceShip: public ObjectBase{
 
 	protected:
-		SDL_Surface * ship_obj;
-		SDL_Surface * screen; // A janela principal
-		std::string ship_name;
-		std::string ship_image;
 		std::vector<SpaceShip *> enemies;
-		int pos_x;
-		int pos_y;
 		int vel_x;
 		int vel_y;
-		SDL_Rect dest;
 		int life;
 		int shot_value;
 	public:
-		int getPosX();
-		int getPosY();
-		int getWidth();
-		int getHeight();
-		void setPosX(int n_pos_x);
-		void setPosY(int n_pos_y);
 		void setVelX(int n_vel_x);
 		void setVelY(int n_vel_y);
-		void setScreen(SDL_Surface * n_screen);
 		void setShipName(std::string n_ship_name);
 		std::string getShipName();
 		void setImageShip(std::string n_ship_image);
@@ -37,10 +24,7 @@ class SpaceShip{
 		void setLife(int n_tot_life);
 		void setShotValue(int n_shot_value);
 		int gotShot();
-		void moveX(int n_vel_x);
-		void moveY(int n_vel_y);
 		void update();
-		void draw();
 		void load();
 		void addEnemy(SpaceShip *n_enemy);
 
